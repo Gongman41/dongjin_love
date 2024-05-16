f5910a9690af5cd334c5b8d1a2077f15acca8543
Gongman41 <lkm98401@gmail.com>
2024-05-16 14:40:27 +0900

    데이터 일단 가져오기 성공

--- back/movies/models.py
+++ back/movies/models.py
@@ -7,10 +7,13 @@ class Genre(models.Model):
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
+    # movieNm
     release_date = models.DateField()
+    # openDt
     popularity = models.FloatField()
     vote_count = models.IntegerField()
     vote_average = models.FloatField()
     overview = models.TextField()
     poster_path = models.CharField(max_length=200)
     genres = models.ManyToManyField(Genre)
+    dongjin_point = models.FloatField(default = 5)
--- back/movies/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-from .models import Movie
-# Create your views here.
-# 영화 제목으로 영화 코드 추출 후 상세검색으로 재검색. 이후 리턴값 모델에 저장

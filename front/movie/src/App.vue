399ec265cd0780a7600692daa1841a10b696d87e
이승민 <lsm5631@naver.com>
2024-05-16 15:39:30 +0900

    회원가입 및 로그아웃

--- front/movie/src/App.vue
+++ front/movie/src/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <nav>
+  <nav v-if="memberStore.token !== null">
     <RouterLink to="/">홈으로</RouterLink> |
     <RouterLink to="/dongjinlove">동진이게임</RouterLink> |
-    <RouterLink to="/moviedetail">영화정보</RouterLink> |
+    <RouterLink to="/moviedetail">영화정보</RouterLink>
+    <button @click="logout">logout</button>
+  </nav>
+
+  <nav v-else>
     <RouterLink to="/signup">회원가입</RouterLink> |
     <RouterLink to="/login">로그인</RouterLink>
   </nav>
@@ -14,10 +18,14 @@
 <script setup>
 import { ref } from 'vue'
 import { RouterView, RouterLink } from 'vue-router'
+import { useMemberStore } from './stores/member';
 
+const memberStore = useMemberStore()
+
+const logout = function () {
+  memberStore.logout()
+}
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>

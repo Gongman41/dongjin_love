b07ee948a09b3d80beb5c968a4e850572d045bd4
이승민 <lsm5631@naver.com>
2024-05-17 16:30:49 +0900

    member.js수정 및 개인 게임페이지로 이동

--- front/movie/src/views/DongjinLoveView.vue
+++ front/movie/src/views/DongjinLoveView.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
     <iframe frameborder="0" src="https://itch.io/embed-upload/10418501?color=333333" allowfullscreen="" width="640"
-      height="380"><a href="https://lkm98401.itch.io/test">Play test on itch.io</a></iframe>
-
+      height="380"><a :href="url">Play test on itch.io</a></iframe>
   </div>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue'
-import { useRoute } from 'vue-router';
 import { useMemberStore } from '@/stores/member';
-import axios from 'axios';
 
+const memberStore = useMemberStore()
+const userId = ref(null)
+const url = ref(null)
+onMounted(() => {
+  memberStore.getProfile()
+  userId.value = memberStore.profile.id
+  url.value = `https://lkm98401.itch.io/test?user_id=${userId}`
+})
 
 </script>
 
